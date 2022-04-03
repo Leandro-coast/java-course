5eb9a6c8ae05d17c9e094dadee73e67944f9918f
Leandro <leandro_coast@outlook.com>
2022-04-03 19:07:58 -0300

    polimorfismo

---
+++ src/entities/UsedProduct.java
@@ -0,0 +1,28 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product{
+    private Date manufactureDate;
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public UsedProduct(String name, double price, Date manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName()
+                + "(used) $"
+                + String.format("%.2f", getPrice())
+                + "(Manufacture date: "
+                + sdf.format(manufactureDate)
+                + ")";
+    }
+}
+
+
+
